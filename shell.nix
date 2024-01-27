eecb1bef27aeea8befa7600c9918c95956229fb8
Diratof Azdimarlow <you@example.com>
2024-01-28 01:51:32 +0500

    restructured

--- shell.nix
+++ shell.nix
@@ -1,9 +1,9 @@
 { pkgs ? import <nixpkgs> {} }:
 pkgs.mkShell {
   packages = with pkgs; let 
-    g4f = pkgs.python3Packages.callPackage ./g4f-nix/default.nix {};
-    llm_audit = pkgs.python3Packages.callPackage ./llm_audit/default.nix {g4f = g4f;};
-    llm_audit-vscode = callPackage ./default.nix {llm_audit = llm_audit;};
+    g4f = pkgs.python3Packages.callPackage ./llm_audit.pkgs/g4f-nix/default.nix {};
+    llm_audit = pkgs.python3Packages.callPackage ./llm_audit.pkgs/llm_audit.nix {g4f = g4f;};
+    llm_audit-vscode = callPackage ./llm_audit.pkgs/llm_audit-vscode.nix {llm_audit = llm_audit;};
   in [
     python3Full git
     (vscode-with-extensions.override{vscodeExtensions = [ llm_audit-vscode ];})
